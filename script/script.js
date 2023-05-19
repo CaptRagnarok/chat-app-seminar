619a3c302919530f846db462313e28ec38330c39
Ragnarok <dzankic.90@gmail.com>
2023-05-19 17:25:48 +0200

    attempt at addressing the mobile bar, 7

--- script/script.js
+++ script/script.js
@@ -302,8 +302,9 @@ hamburger.addEventListener('click', function () {
   }
 });
 // document.addEventListener('click', handleDocumentClick); // closes sidebar when you click anywhere, but needs more work
-
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('load', function () {
+  setTimeout(function () {
+    // This hides the address bar:
+    window.scrollTo(0, 1);
+  }, 0);
+});
